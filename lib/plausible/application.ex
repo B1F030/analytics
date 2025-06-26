59240152ef5e14745736e5d4f62c4b51a21cb8a4
B1F030 <b1fzhang@gmail.com>
2025-06-26 20:16:03 +0800

    fix all
    Signed-off-by: B1F030 <b1fzhang@gmail.com>

--- lib/plausible/application.ex
+++ lib/plausible/application.ex
@@ -214,12 +214,6 @@ defmodule Plausible.Application do
       "https://icons.duckduckgo.com",
       Config.Reader.merge(default_opts, conn_opts: [transport_opts: [timeout: 15_000]])
     )
-    # |> Map.put(
-    #   "https://www.googleapis.com",
-    #   Config.Reader.merge(default_opts, 
-    #     conn_opts: [proxy: {:http, 'gfwproxy.infra.svc.cluster.local', 1080}]
-    #   )
-    # )
     |> maybe_add_sentry_pool(default_opts)
     |> maybe_add_paddle_pool(default_opts)
     |> maybe_add_google_pools(default_opts)
@@ -266,13 +260,6 @@ defmodule Plausible.Application do
           google_conf[:reporting_api_url],
           Config.Reader.merge(default, conn_opts: [transport_opts: [timeout: 15_000]])
         )
-        # |> Map.put(
-        #   "https://www.googleapis.com",
-        #   Config.Reader.merge(default, conn_opts: [
-        #     proxy: {:http, 'gfwproxy.infra.svc.cluster.local', 1080, []},
-        #     transport_opts: [timeout: 15_000]
-        #   ])
-        #   )
       true ->
         pool_config
     end
